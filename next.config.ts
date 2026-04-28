b4835c710c3fdddafe729e7aa0bcbb7d4dea1efa
Apple <apple@appurunoMacBook-Pro.local>
2026-04-28 16:39:38 +0900

    Reset next.config.ts for troubleshooting

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === 'development';
-
 const nextConfig: NextConfig = {
   /* config options here */
-  ...(isDev ? {
-    allowedDevOrigins: ['192.168.0.7', 'localhost:3000'],
-  } : {}),
 };
 
 export default nextConfig;
